ca559ea4eee3f07eca787bea68e7e9f736608505
Anisio Ferreira Suitsu <anisioajr@gmail.com>
2025-10-05 22:58:11 -0300

    initial commit primeira

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/anisio/rest_with_spring_boot_and_java/Startup.java
@@ -0,0 +1,13 @@
+package br.com.anisio.rest_with_spring_boot_and_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/anisio/rest_with_spring_boot_and_java/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.anisio.rest_with_spring_boot_and_java.model;
+
+public record Greeting(long id, String content) {
+
+}
